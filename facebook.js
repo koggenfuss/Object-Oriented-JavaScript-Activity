e4ea91abc5bd7745839596c7575f93e42e508821
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-01-20 22:05:52 -0500

    update addComment and time functions

---
+++ facebook.js
@@ -0,0 +1,34 @@
+let post = {
+    name: "Katharyn Lynn Oggenfuss",
+    tag: "Code Differently",
+    message: "Code Differently is the best!",
+    time: "7:20",
+    reaction: ["thumbs up", "love", "thumbs down", "angry", "shocked"],
+    picture: "image",
+    caption : "Coding is awesome!",
+    comment: "that's great!",
+    share: "share with friends",
+    
+    addComment(comment) {
+        this.comment = comment;
+        return comment;
+    },
+
+    time() {
+        var today = new Date();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        return time;
+    },
+
+    react(feeling) {
+        for( let i=0; i < reaction.length; i++) {
+            if (feeling === reaction[i]) {
+                return reaction[i];
+            }
+        }
+    }
+}
+
+console.log(post.addComment("This is my first comment"));
+console.log(post.time());
+console.log(post.feeling());
